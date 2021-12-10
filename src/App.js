2808114f291fb150f02e1a7e5fbf0c6dce49591a
Fabian Bariandaran <fabianbdr@icloud.com>
2021-12-09 21:12:35 -0300

    Primer push a main

--- src/App.js
+++ src/App.js
@@ -7,16 +7,15 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Bienvenidos a mi aplicación en React!
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://github.com/fabiancitonline/react-bariandaran"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
-        </a>
+BARIANDARAN-REACT</a>
       </header>
     </div>
   );
